4dec3889f5ec3eb6c7fd4f14e0eb18c5b9263b99
Analia Mega <analiamega@gmail.com>
2020-11-26 17:59:15 -0300

    Inicial commit tp-angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,33 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { ProductCartComponent } from './product-cart/product-cart.component';
+import { FormsModule } from '@angular/forms';
+import { ContactComponent } from './contact/contact.component';
+import { ProductosComponent } from './productos/productos.component';
+import { InputCantidadComponent } from './input-cantidad/input-cantidad.component';
+
+
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  declarations: [
+    AppComponent,
+    ProductListComponent,
+    ProductCartComponent,
+    ContactComponent,
+    ProductosComponent,
+    InputCantidadComponent,
+
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
